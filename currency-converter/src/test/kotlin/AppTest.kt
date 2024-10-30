f5a57de7ebdcbd1473c03f3094f206bc05bc74d9
tazzledazzle <terenceschumacher@gmail.com>
2024-10-30 13:37:07 -0700

    F** sealed class conversion tests updated

--- currency-converter/src/test/kotlin/AppTest.kt
+++ currency-converter/src/test/kotlin/AppTest.kt
@@ -1,10 +1,24 @@
+import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertNotNull
 import kotlin.test.Test
+import kotlin.test.fail
 
 class AppTest {
     @Test
     fun testAppHasAGreeting() {
         val classUnderTest = App()
         assertNotNull("app should have a greeting", classUnderTest.greeting)
+    }
+
+    @Test
+    fun testConversionAmount() {
+        val amountInUSD = 100.0
+        when (val result = amountInUSD.convert(Currency.USD, Currency.EUR)) {
+            is ConversionResult.Success -> {
+                assertEquals(85.0, result.amount)
+                assertEquals(Currency.EUR, result.currency)
+            }
+            else -> fail("Conversion failed")
+        }
     }
 }
